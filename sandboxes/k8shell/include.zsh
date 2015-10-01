276ef8900aa608059ab116ef880f1b9f975f4466
Mark Mandel <markmandel@google.com>
2015-10-01 11:15:20 -0700

    Virtualbox driver is now being loaded.

--- sandboxes/k8shell/include.zsh
+++ sandboxes/k8shell/include.zsh
@@ -1,12 +1,13 @@
 #!/usr/bin/env zsh
 
 #includes for k8s zsh config
-function k8sshell() {
+function k8shell() {
     _docker_zsh "k8shell" "/project" \
         "--privileged" \
         "-e DISPLAY" \
         "-e QT_GRAPHICSSYSTEM=native" \
         "-e XAUTHORITY" \
+        "-v /dev/:/dev/" \
         "-v $HOME/.Xauthority:$HOME/.Xauthority" \
         "-v /tmp/.X11-unix:/tmp/.X11-unix"
 }
--- sandboxes/k8shell/startup.sh
+++ sandboxes/k8shell/startup.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env sh
 groupadd --gid $HOST_GID $HOST_USER
 useradd $HOST_USER --home /home/$HOST_USER --gid $HOST_GID --uid $HOST_UID --shell /usr/bin/zsh
+echo "$HOST_USER:pw" | chpasswd
 chown $HOST_USER:$HOST_USER /home/$HOST_USER
+adduser $HOST_USER sudo
+
+if [ ! -f /dev/vboxdrv ]; then
+    echo "Could not find VirtualBox Driver. Ooops. Setting up...."
+    /etc/init.d/vboxdrv setup
+else
+    echo "Found VirtualBox Driver."
+fi
 
 su $HOST_USER
