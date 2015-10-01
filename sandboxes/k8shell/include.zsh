5ff4dce9aacf11b917e128cc1ce54769fda0d362
Mark Mandel <markmandel@google.com>
2015-09-30 21:04:51 -0700

    First pass at getting virtualbox installed in a container.

---
+++ sandboxes/k8shell/include.zsh
@@ -0,0 +1,12 @@
+#!/usr/bin/env zsh
+
+#includes for k8s zsh config
+function k8sshell() {
+    _docker_zsh "k8shell" "/project" \
+        "--privileged" \
+        "-e DISPLAY" \
+        "-e QT_GRAPHICSSYSTEM=native" \
+        "-e XAUTHORITY" \
+        "-v $HOME/.Xauthority:$HOME/.Xauthority" \
+        "-v /tmp/.X11-unix:/tmp/.X11-unix"
+}
