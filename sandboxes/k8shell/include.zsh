e948e57bd38a90d1590f520608c7f5574055f876
Mark Mandel <markmandel@google.com>
2015-10-01 20:35:36 -0700

    Before I switch over to the different version

--- sandboxes/k8shell/include.zsh
+++ sandboxes/k8shell/include.zsh
@@ -2,12 +2,27 @@
 
 #includes for k8s zsh config
 function k8shell() {
+    set -x
+
+    local mount="-v /dev/:/dev/"
+    mkdir -p ~/.vagrant.d
+
+    if [ -e /dev/vboxdrv ]; then
+        mount="-v /dev/vboxdrv:/dev/vboxdrv"
+    fi
+
+    echo "Mount: $mount"
+
     _docker_zsh "k8shell" "/project" \
+        "--net=host" \
         "--privileged" \
         "-e DISPLAY" \
         "-e QT_GRAPHICSSYSTEM=native" \
         "-e XAUTHORITY" \
-        "-v /dev/:/dev/" \
+        $mount \
         "-v $HOME/.Xauthority:$HOME/.Xauthority" \
-        "-v /tmp/.X11-unix:/tmp/.X11-unix"
+        "-v /tmp/.X11-unix:/tmp/.X11-unix" \
+        "-v $HOME/.vagrant.d:$HOME/.vagrant.d"
+
+        set +x
 }
--- sandboxes/k8shell/startup.sh
+++ sandboxes/k8shell/startup.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env sh
+echo "Adding user $HOST_USER"
 groupadd --gid $HOST_GID $HOST_USER
 useradd $HOST_USER --home /home/$HOST_USER --gid $HOST_GID --uid $HOST_UID --shell /usr/bin/zsh
 echo "$HOST_USER:pw" | chpasswd
-chown $HOST_USER:$HOST_USER /home/$HOST_USER
 adduser $HOST_USER sudo
+cp -r /kubernetes /home/$HOST_USER
+chown -R $HOST_USER:$HOST_USER /home/$HOST_USER
 
 if [ -e /dev/vboxdrv ]; then
-    echo "Found VirtualBox Driver."
+    echo "Found VirtualBox Driver. Continuing..."
 else
     echo "Could not find VirtualBox Driver. Ooops. Setting up...."
     /etc/init.d/vboxdrv setup
+    echo "Exiting after setup"
+    exit
 fi
 
+echo "Switch user muther fucker"
 su $HOST_USER
