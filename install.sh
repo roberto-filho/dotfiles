2b0de3631540cc0c87dc655ec0cdb79d3d569b6f
Roberto Filho <roberto.w.filho@gmail.com>
2017-02-28 01:24:12 -0300

    Trying out my version.

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
-sudo apt-get install software-properties-common
-sudo apt-add-repository ppa:ansible/ansible
+sudo apt-get install -y software-properties-common
+sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
-sudo apt-get install ansible git
+sudo apt-get install -y ansible git
 cd ~
 
 if [ ! -d "~/workspace/dotfiles" ]; then
     mkdir workspace
     cd workspace
-    git clone git@github.com:markmandel/dotfiles.git
+    git clone git@github.com:roberto-filho/dotfiles.git
 fi
